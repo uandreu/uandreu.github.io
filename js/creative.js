af1442873ff7c222e7418393757b6280507436f7
uandreu <auwl2016@gmail.com>
2019-05-19 09:00:18 -0400

    Beginning to implement login system of some kind so I can also use this as a home page lol

--- js/creative.js
+++ js/creative.js
@@ -60,6 +60,26 @@
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
     });
+	
+	$(document).ready(function() {
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+			// When elemened is focused, some mobile browsers in some cases zoom in
+			// It looks not nice, so we disable it:
+			callbacks: {
+				beforeOpen: function() {
+					if($(window).width() < 700) {
+						this.st.focus = false;
+					} else {
+						this.st.focus = '#name';
+					}
+				}
+			}
+		});
+	});
 
 })(jQuery); // End of use strict
 
